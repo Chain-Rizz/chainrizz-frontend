ec528075062d73a1ef8975e10cc00871c0039262
Prathamesh Mutkure <pmutkure009@gmail.com>
2024-10-13 09:32:04 +0100

    PayPal UI Added

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -24,7 +24,8 @@ import { toast } from "@/hooks/use-toast";
 import { axiosInstance } from "@/lib/axios";
 import processTransaction from "@/lib/bridge";
 import { useRouter, useSearchParams } from "next/navigation";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import dropin, { Dropin } from "braintree-web-drop-in";
 
 export default function CardsPaymentMethod() {
   const router = useRouter();
@@ -40,6 +41,8 @@ export default function CardsPaymentMethod() {
   const [trackingId, setTrackingId] = useState<null | string>(
     searchParams.get("trackingId") ?? null
   );
+
+  const [mode, setMode] = useState("usdc");
 
   const [isPaymentProcessing, setIsPaymentProcessing] = useState(false);
 
@@ -72,6 +75,97 @@ export default function CardsPaymentMethod() {
     }
   }
 
+  const [clientToken, setClientToken] = useState<string | null>(null);
+  const [dropinInstance, setDropinInstance] = useState<Dropin | null>(null);
+
+  const [fiatAmount, setFiatAmount] = useState(0);
+  const [stellarHash, setStellarHash] = useState<null | string>(null);
+
+  useEffect(() => {
+    async function initDropIn() {
+      if (!clientToken) return;
+
+      const dropinInstance = await dropin.create({
+        authorization: clientToken,
+        container: "#dropin-container-div",
+        paypal: {
+          flow: "checkout",
+          currency: "INR",
+        },
+      });
+
+      setDropinInstance(dropinInstance);
+    }
+
+    initDropIn();
+  }, [clientToken]);
+
+  async function handlePaypalPayment() {
+    if (!dropinInstance) return;
+
+    try {
+      setIsPaymentProcessing(true);
+
+      if (fiatAmount <= 0 || fiatAmount > 0.5) {
+        toast({
+          title: "Invalid amount",
+          variant: "destructive",
+          description:
+            "For testing purposes, we have restricted the amount between 0.01-0.5",
+        });
+
+        return;
+      }
+
+      if (!toAddress) {
+        toast({
+          title: "Invalid address",
+          variant: "destructive",
+          description: "Please enter a valid address",
+        });
+
+        return;
+      }
+
+      const paymentPayload = await dropinInstance.requestPaymentMethod();
+
+      const { nonce } = paymentPayload;
+
+      const payload = {
+        nonce,
+        toAddress: toAddress,
+        tokenAmount,
+        trackingId,
+      };
+
+      const res = await axiosInstance.post("/paypal/checkout", payload);
+
+      const data = res.data as {
+        message: string;
+        id: string;
+        txHash: string;
+        gatewayId: string;
+      };
+
+      setStellarHash(data.txHash);
+
+      toast({
+        title: data.message,
+        description: `Transaction Hash: ${data.txHash}`,
+      });
+
+      // router.replace(`/dashboard/transactions`);
+    } catch (err) {
+      console.log(err);
+      toast({
+        title: "An error occurred.",
+        variant: "destructive",
+      });
+    } finally {
+      setIsPaymentProcessing(false);
+    }
+  }
+
   return (
     <div className="flex items-center justify-center h-screen overflow-x-auto p-4">
       <Card className="w-full max-w-screen-sm">
@@ -83,10 +177,19 @@ export default function CardsPaymentMethod() {
         </CardHeader>
 
         <CardContent className="grid gap-6">
-          <RadioGroup defaultValue="card" className="grid grid-cols-3 gap-4">
+          <RadioGroup
+            defaultValue="card"
+            className="grid grid-cols-3 gap-4"
+            onChange={(e) => {
+              console.log(e.currentTarget.nodeValue);
+            }}
+            onValueChange={(val) => {
+              setMode(val);
+            }}
+          >
             <div>
               <RadioGroupItem
-                value="card"
+                value="usdc"
                 id="card"
                 className="peer sr-only"
                 aria-label="Card"
@@ -108,7 +211,7 @@ export default function CardsPaymentMethod() {
                   <rect width="20" height="14" x="2" y="5" rx="2" />
                   <path d="M2 10h20" />
                 </svg>
-                Card
+                USDC
               </Label>
             </div>
 
@@ -130,7 +233,7 @@ export default function CardsPaymentMethod() {
 
             <div>
               <RadioGroupItem
-                value="apple"
+                value="xlm"
                 id="apple"
                 className="peer sr-only"
                 aria-label="Apple"
@@ -145,50 +248,60 @@ export default function CardsPaymentMethod() {
             </div>
           </RadioGroup>
 
-          <div className="grid gap-2">
-            <Label htmlFor="month">Chain</Label>
-            <Select defaultValue="1">
-              <SelectTrigger id="month" aria-label="Month">
-                <SelectValue placeholder="Chain" />
-              </SelectTrigger>
-              <SelectContent>
-                <SelectItem value="1">Arbitrum</SelectItem>
-                <SelectItem value="2">Etereum</SelectItem>
-                <SelectItem value="3">Polygon</SelectItem>
-                <SelectItem value="4">Base</SelectItem>
-              </SelectContent>
-            </Select>
-          </div>
+          {mode === "usdc" && (
+            <>
+              <div className="grid gap-2">
+                <Label htmlFor="month">Chain</Label>
+                <Select defaultValue="1">
+                  <SelectTrigger id="month" aria-label="Month">
+                    <SelectValue placeholder="Chain" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectItem value="1">Arbitrum</SelectItem>
+                    <SelectItem value="2">Etereum</SelectItem>
+                    <SelectItem value="3">Polygon</SelectItem>
+                    <SelectItem value="4">Base</SelectItem>
+                  </SelectContent>
+                </Select>
+              </div>
 
-          <div className="grid gap-2">
-            <Label htmlFor="city">Receipent Address</Label>
-            <Input
-              id="city"
-              placeholder="0x123..."
-              value={toAddress}
-              onChange={(e) => setToAddress(e.target.value)}
-            />
-          </div>
+              <div className="grid gap-2">
+                <Label htmlFor="city">Receipent Address</Label>
+                <Input
+                  id="city"
+                  placeholder="0x123..."
+                  value={toAddress}
+                  onChange={(e) => setToAddress(e.target.value)}
+                />
+              </div>
 
-          <div className="grid gap-2">
-            <Label htmlFor="number">Amount</Label>
-            <Input
-              id="number"
-              placeholder="Amount in USDC"
-              value={tokenAmount}
-              onChange={(e) => setTokenAmount(parseFloat(e.target.value))}
-            />
-          </div>
+              <div className="grid gap-2">
+                <Label htmlFor="number">Amount</Label>
+                <Input
+                  id="number"
+                  placeholder="Amount in USDC"
+                  value={tokenAmount}
+                  onChange={(e) => setTokenAmount(parseFloat(e.target.value))}
+                />
+              </div>
 
-          <div className="grid gap-2">
-            <Label htmlFor="name">Email</Label>
-            <Input
-              id="email"
-              placeholder="Email"
-              defaultValue="user@gmail.com"
-              onChange={(e) => setToAddress(e.target.value)}
-            />
-          </div>
+              <div className="grid gap-2">
+                <Label htmlFor="name">Email</Label>
+                <Input
+                  id="email"
+                  placeholder="Email"
+                  defaultValue="user@gmail.com"
+                  onChange={(e) => setToAddress(e.target.value)}
+                />
+              </div>
+            </>
+          )}
+
+          {mode === "paypal" && <>{!clientToken && <p>Loading...</p>}</>}
+
+          {mode === "paypal" && clientToken && (
+            <div id="dropin-container-div"></div>
+          )}
         </CardContent>
         <CardFooter>
           <Button className="w-full" onClick={handlePayment}>
