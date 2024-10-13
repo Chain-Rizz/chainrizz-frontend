65d7e0f3fea802a8b13342290f62b8621c8b7961
Prathamesh Mutkure <pmutkure009@gmail.com>
2024-10-13 03:18:39 +0100

    Integrated AllBridge

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,87 @@
-import Image from "next/image";
+"use client";
+
+import { Button } from "@/components/ui/button";
+
+import {
+  AllbridgeCoreSdk,
+  ChainSymbol,
+  Messenger,
+  nodeRpcUrlsDefault,
+  RawEvmTransaction,
+} from "@allbridge/bridge-core-sdk";
+import Web3 from "web3";
+
+import { getEnvVar } from "@/lib/env";
+import { sendEvmRawTransaction } from "@/lib/web3";
+import { ensure } from "@/lib/utils";
 
 export default function Home() {
+  async function onClick(from: string, to: string, amount: string) {
+    const fromAddress = to; // sender address
+    const toAddress = to; // recipient address
+
+    const sdk = new AllbridgeCoreSdk({
+      ...nodeRpcUrlsDefault,
+      // ETH: getEnvVar("WEB3_PROVIDER_URL"),
+      ARB: "https://arbitrum-mainnet.infura.io/v3/9c923f4f0b0a4278bb83849196a5cdf8",
+    });
+
+    const chains = await sdk.chainDetailsMap();
+
+    const sourceChain = chains[ChainSymbol.ARB];
+    const sourceToken = ensure(
+      sourceChain.tokens.find((tokenInfo) => tokenInfo.symbol === "USDC")
+    );
+
+    const destinationChain = chains[ChainSymbol.SRB];
+    const destinationToken = ensure(
+      destinationChain.tokens.find((tokenInfo) => tokenInfo.symbol === "USDC")
+    );
+
+    //check if sending tokens already approved
+    if (
+      !(await sdk.bridge.checkAllowance({
+        token: sourceToken,
+        owner: fromAddress,
+        amount: amount,
+      }))
+    ) {
+      console.log("Approving tokens");
+
+      // authorize the bridge to transfer tokens from sender's address
+      const rawTransactionApprove = (await sdk.bridge.rawTxBuilder.approve({
+        token: sourceToken,
+        owner: fromAddress,
+      })) as RawEvmTransaction;
+
+      console.log("Approve tx:", rawTransactionApprove);
+
+      const approveTxReceipt = await sendEvmRawTransaction(
+        rawTransactionApprove
+      );
+
+      console.log("Approve tx id:", approveTxReceipt.transactionHash);
+    }
+
+    // initiate transfer
+    const rawTransactionTransfer = (await sdk.bridge.rawTxBuilder.send({
+      amount: amount,
+      fromAccountAddress: fromAddress,
+      toAccountAddress: toAddress,
+      sourceToken: sourceToken,
+      destinationToken: destinationToken,
+      messenger: Messenger.ALLBRIDGE,
+    })) as RawEvmTransaction;
+    console.log(`Sending ${amount} ${sourceToken.symbol}`);
+    const txReceipt = await sendEvmRawTransaction(rawTransactionTransfer);
+    console.log("tx id:", txReceipt);
+  }
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
+        <Button onClick={onClick}>Test</Button>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
---
+++ src/lib/web3.ts
@@ -0,0 +1,35 @@
+import Web3 from "web3";
+import { TransactionConfig } from "web3-core";
+import { getEnvVar } from "./env";
+
+export async function sendEvmRawTransaction(rawTransaction: TransactionConfig) {
+  // configure web3
+  const web3 = new Web3(
+    "https://arbitrum-mainnet.infura.io/v3/9c923f4f0b0a4278bb83849196a5cdf8"
+  );
+  const account = web3.eth.accounts.privateKeyToAccount(
+    "0x9bb6ca3ff73a4b99f1dab52b9b3bda3b57d5dfb3275d6119862a358cacb6d717"
+  );
+  web3.eth.accounts.wallet.add(account);
+
+  if (rawTransaction.from === undefined) {
+    throw Error("rawTransaction.from is undefined");
+  }
+  const gasLimit = await web3.eth.estimateGas(rawTransaction);
+  const gasPrice = await web3.eth.getGasPrice();
+
+  //   console.log("Gas limit", gasLimit);
+  //   console.log("Gas Price", gasPrice);
+
+  const signedTx = await account.signTransaction({
+    ...rawTransaction,
+    gas: gasLimit,
+    gasPrice: gasPrice,
+    chain: "sepolia",
+  });
+  if (signedTx.rawTransaction === undefined) {
+    throw Error("signedTx.rawTransaction is undefined");
+  }
+  console.log("Sending transaction", signedTx.transactionHash);
+  return web3.eth.sendSignedTransaction(signedTx.rawTransaction);
+}
